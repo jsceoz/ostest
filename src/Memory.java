d88a0764e46f90816cd7cb54026ff9850936ac27
Jsceoz <jsceoz96@gmail.com>
2016-12-18 20:24:29 +0800

    #2

---
+++ src/Memory.java
@@ -0,0 +1,25 @@
+public class Memory {
+    int length;
+    MemoryBlock[] memoryMap;
+
+    public void insertRequest(Request work) {
+
+    }
+
+    public void deleteRequest(Request work) {
+
+    }
+
+    public void printMemoryMap() {
+
+    }
+
+    public void createNewMemoryBlock(int size) {
+
+    }
+
+    public void mergeFreeMemoryBlock() {
+
+    }
+
+}
---
+++ src/MemoryBlock.java
@@ -0,0 +1,5 @@
+public class MemoryBlock {
+    int length;
+    int state;
+    String workName;
+}
