cf97151f93d9c61e4d5df06a0319b22e6f32e9dd
Jsceoz <jsceoz96@gmail.com>
2016-12-18 22:51:17 +0800

    #2

--- src/Memory.java
+++ src/Memory.java
@@ -1,25 +1,49 @@
+import java.util.ArrayList;
+
 public class Memory {
-    int length;
-    MemoryBlock[] memoryMap;
+    private int length;
+    private ArrayList<MemoryBlock> MemoryBlocks;
 
     public void insertRequest(Request work) {
 
     }
 
     public void deleteRequest(Request work) {
-
+        int i = 0;
+        while (MemoryBlocks.get(i).getWorkName() != work) {
+            i++;
+        }
+        MemoryBlocks.get(i).setWorkName(null);
     }
 
     public void printMemoryMap() {
-
+        for (MemoryBlock mb :
+                MemoryBlocks) {
+            System.out.print(mb.getStart());
+            System.out.print("/");
+            System.out.print(mb.getLength());
+            System.out.print("/");
+            System.out.print(mb.getState());
+            System.out.print("/");
+            System.out.print(mb.getWorkName());
+        }
     }
 
     public void createNewMemoryBlock(int size) {
-
+        MemoryBlock mb = new MemoryBlock(size);
+        MemoryBlocks.add(mb);
     }
 
-    public void mergeFreeMemoryBlock() {
+    public void mergeFreeMemoryBlock(MemoryBlock mb1, MemoryBlock mb2) {
+        MemoryBlock mb = addMemoryBlcok(mb1, mb2);
+        MemoryBlocks.add(MemoryBlocks.indexOf(mb1), mb);
+        MemoryBlocks.remove(mb1);
+        MemoryBlocks.remove(mb2);
+    }
 
+    private MemoryBlock addMemoryBlcok(MemoryBlock mb1, MemoryBlock mb2) {
+        int length = mb1.getLength() + mb2.getLength();
+        return new MemoryBlock(length);
     }
 
 }
--- src/MemoryBlock.java
+++ src/MemoryBlock.java
@@ -1,5 +1,40 @@
 public class MemoryBlock {
-    int length;
-    int state;
-    String workName;
+    private int start;
+    private int length;
+    private int state;
+    private Request workName;
+
+    public MemoryBlock(int length) {
+        this.length = length;
+        this.state = 0;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getState() {
+        return state;
+
+    }
+
+    public Request getWorkName() {
+        return workName;
+    }
+
+    public void setWorkName(Request work) {
+        this.workName = workName;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
 }
